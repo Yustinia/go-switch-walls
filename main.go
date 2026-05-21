ec4d48cf2ab2682facc6ac322434906d4e41d404
Yustinia <yustinia@protonmail.com>
2026-05-21 12:20:27 +0800

    render test

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	tea "charm.land/bubbletea/v2"
+)
+
+type model struct {
+	walls []string
+}
+
+func allocateList() model {
+	return model{walls: []string{"Test1", "Test2"}}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	msg := m.walls[0]
+	return tea.NewView(msg)
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	p := tea.NewProgram(allocateList())
+	if _, err := p.Run(); err != nil {
+		os.Exit(1)
+	}
 }
